24d6e0f8e67a0bc251e62d9aa48038df5de01b13
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-08-07 18:57:49 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,6 +53,8 @@ holiday_supplies[:winter].values.flatten
 end 
 
 def all_supplies_in_holidays(holiday_hash)
+
+
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
